22e71b11853d64b27edc4eaff25f717244dac501
xin <2512208947@qq.com>
2022-12-24 18:39:47 +0800

    初始化工程

--- main.go
+++ main.go
@@ -1,12 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go-redis/config"
+	"go-redis/lib/logger"
+	"go-redis/tcp"
+	EchoHandler "go-redis/tcp"
+	"os"
+)
 
-/**
- * @author 辛老板
- * @date 2022/12/23 21:16
- * @version 1.0.0
- */
+const configFile string = "redis.conf"
+
+var defaultProperties = &config.ServerProperties{
+	Bind: "0.0.0.0",
+	Port: 6379,
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	return err == nil && !info.IsDir()
+}
+
 func main() {
-	fmt.Println("main go")
+	logger.Setup(&logger.Settings{
+		Path:       "logs",
+		Name:       "godis",
+		Ext:        "log",
+		TimeFormat: "2006-01-02",
+	})
+
+	if fileExists(configFile) {
+		config.SetupConfig(configFile)
+	} else {
+		config.Properties = defaultProperties
+	}
+
+	err := tcp.ListenAndServeWithSignal(
+		&tcp.Config{
+			Address: fmt.Sprintf("%s:%d",
+				config.Properties.Bind,
+				config.Properties.Port),
+		},
+		EchoHandler.MakeHandler())
+	if err != nil {
+		logger.Error(err)
+	}
 }
